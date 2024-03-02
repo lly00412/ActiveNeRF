351d451618893e4edffd7a8f50da40275a50a176
lly00412 <315885469@qq.com>
2024-03-01 19:35:47 -0500

    make some changes

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+export ROOT_DIR=/mnt/Data2/nerf_datasets/nerf_synthetic/
+scenes=(chair drums ficus)
+
+for SCENES in ${scenes[@]}
+do
+echo ${SCENES}
 python run_nerf.py --config configs/hotdog_active.txt \
---expname active_hotdog2 \
+--expname active_${SCENES} \
 --basedir ./logs \
---datadir /mnt/Data2/datasets/nerf_synthetic/hotdog/ \
---jsondir ./data/hotdog \
+--datadir ${ROOT_DIR}/${SCENES}/ \
+--jsondir ./data/${SCENES} \
 --choose_k 1
+
+done
